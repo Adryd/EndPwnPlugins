d86971570e36b84e7c85c90272a3dcdbeb3d4379
Adryd <Adryd@users.noreply.github.com>
2018-05-28 11:33:29 -0400

    Forgot to remove function that added css
    aaa

--- plugins/guildcount.js
+++ plugins/guildcount.js
@@ -4,10 +4,11 @@ function formattedGuildCount(shortened) {
 }
 
 function setupServerCount() {
-    if (document.querySelector(".server-count")) return;
+    if (document.querySelector("#server-count")) return;
 
     let gc = document.createElement("div");
-    gc.className = "server-count";
+    gc.className = "friends-online";
+    gc.id = "server-count"
 
     let glist = document.querySelector(".friends-online");
     if (!glist) return;
@@ -16,27 +17,8 @@ function setupServerCount() {
   }
 
 function updateServerCount() {
-    if (document.querySelector(".server-count") && document.querySelector(".server-count").innerHTML == formattedGuildCount()) return;
-    document.querySelector(".server-count").innerHTML = formattedGuildCount();
-}
-
-function scCSS() {
-    let css = document.createElement("style");
-    css.type = "text/css";
-    css.id = "css-scount";
-    css.innerHTML = `.server-count {
-    color: hsla(0,0%,100%,.3);
-    font-size: 10px;
-    font-weight: 500;
-    line-height: 130%;
-    margin: 10px 0;
-    text-align: center;
-    text-transform: uppercase;
-    white-space: nowrap;
-    width: 50px;
-    word-wrap: normal;
-}`;
-    document.body.appendChild(css);
+    if (document.querySelector("#server-count") && document.querySelector("#server-count").innerHTML == formattedGuildCount()) return;
+    document.querySelector("#server-count").innerHTML = formattedGuildCount();
 }
 
 exports.manifest = {
@@ -45,7 +27,6 @@ exports.manifest = {
     description: "Shows guild count under online count."
 }
 exports.start = () => {
-    scCSS();
 
     let observer = new MutationObserver(() => {
         setupServerCount();
